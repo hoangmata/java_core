259c52e9a3383043ac0fb75a072341cb41b44e9a
hoang1306 <hoang.mata.130@gmail.com>
2024-03-07 11:52:50 +0700

    start maven

---
+++ maven/my-app/README.md
@@ -0,0 +1,3 @@
+<div>
+    <a href="https://maven.apache.org/guides/getting-started/maven-in-five-minutes.html" target="_blank">Guide Maven</a>
+</div>
---
+++ maven/my-app/src/main/java/edu/fa/App.java
@@ -0,0 +1,13 @@
+package edu.fa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Maven!" );
+    }
+}
